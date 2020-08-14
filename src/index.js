a0b1cd06a7a0dc904523f9707b9aa4ef35878b6c
Anton Yulbarysau <io57196657@gmail.com>
2020-08-14 23:39:33 +0300

    add: components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './normalize.css';
+import './style.css';
 
 // const Header = ({ text }) => <h1 id="hi">{text}</h1>;
 
@@ -19,7 +21,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
